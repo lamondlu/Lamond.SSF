0dfe54bc72021c48d4065940191b8fb6bc52a408
Lamond Lu <lunan@shintechchina.com>
2020-08-17 15:42:44 +0800

    update the namespace

---
+++ src/Lamond.SSF.AspNetCore/Extensions/SSFServiceExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Lamond.SSF.Core.Abstract.Domain;
+using Lamond.SSF.Core.Concrete.Domain;
+using Lamond.SSF.Core.Models;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Lamond.SSF.AspNetCore.Extensions
+{
+    public static class SSFServiceExtensions
+    {
+        public static void AddSSF(this IServiceCollection services)
+        {
+            services.AddScoped<ICommandBus, CommandBus>();
+        }
+
+        public static void AddSSF(this IServiceCollection services, Action<SSFOption> configureAction)
+        {
+
+        }
+    }
+}
--- src/Lamond.SSF.Sample/Startup.cs
+++ src/Lamond.SSF.Sample/Startup.cs
@@ -1,4 +1,4 @@
-using Lamond.SSF.AspNetCore;
+using Lamond.SSF.AspNetCore.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
